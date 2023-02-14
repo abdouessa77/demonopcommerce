a37521979b47bfe1e625826bdd3b1fb4925fba40
abdouessa77 <32675087+abdouessa77@users.noreply.github.com>
2023-02-14 02:19:23 +0200

    Prepare and screenshot

---
+++ src/test/java/org/demonopcommerce/stepDefs/D01_registerStepDef.java
@@ -0,0 +1,10 @@
+package org.demonopcommerce.stepDefs;
+import io.cucumber.java.en.Given;
+public class D01_registerStepDef {
+    @Given("user go to register page")
+    public void goRegisterPage()
+    {
+        System.out.println("This is a test before start coding");
+    }
+}
+
